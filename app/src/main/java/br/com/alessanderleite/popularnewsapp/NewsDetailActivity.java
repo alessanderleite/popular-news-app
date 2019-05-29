917784e4d4f9a4b5c3d8eb87f4b63888ae569d77
alessanderleite <alessander.logistica@gmail.com>
2019-05-29 17:18:41 -0300

    call the getIntent() to retrieve data in the NewsDetailActivity

--- app/src/main/java/br/com/alessanderleite/popularnewsapp/NewsDetailActivity.java
+++ app/src/main/java/br/com/alessanderleite/popularnewsapp/NewsDetailActivity.java
@@ -1,5 +1,6 @@
 package br.com.alessanderleite.popularnewsapp;
 
+import android.content.Intent;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -13,11 +14,15 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
+
 public class NewsDetailActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener{
 
     private ImageView imageView;
     private TextView appbar_title, appbar_subtitle, date, time, title;
-    private boolean isHideTolbarView = false;
+    private boolean isHideToolbarView = false;
     private FrameLayout date_behavior;
     private LinearLayout titleAppbar;
     private AppBarLayout appBarLayout;
@@ -47,6 +52,39 @@ public class NewsDetailActivity extends AppCompatActivity implements AppBarLayou
         date = findViewById(R.id.date);
         time = findViewById(R.id.time);
         title = findViewById(R.id.title);
+
+        Intent intent = getIntent();
+        mUrl = intent.getStringExtra("url");
+        mImg = intent.getStringExtra("img");
+        mTitle = intent.getStringExtra("title");
+        mDate = intent.getStringExtra("date");
+        mSource = intent.getStringExtra("source");
+        mAuthor = intent.getStringExtra("author");
+
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.error(Utils.getRandomDrawableColor());
+
+        Glide.with(this)
+                .load(mImg)
+                .apply(requestOptions)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(imageView);
+
+        appbar_title.setText(mSource);
+        appbar_subtitle.setText(mUrl);
+        date.setText(Utils.DateFormat(mDate));
+        title.setText(mTitle);
+
+        String author = null;
+        if (mAuthor != null || mAuthor != "") {
+            mAuthor = " \u2022 " + mAuthor;
+        } else {
+            author = "";
+        }
+
+        time.setText(mSource + author + " \u2022 " + Utils.DateToTimeFormat(mDate));
+
+        initWebView(mUrl);
     }
 
     private void initWebView(String url) {
@@ -79,16 +117,16 @@ public class NewsDetailActivity extends AppCompatActivity implements AppBarLayou
         int maxScroll = appBarLayout.getTotalScrollRange();
         float percentage = (float) Math.abs(verticalOffset) / (float) maxScroll;
 
-        if (percentage == 1f && isHideTolbarView) {
+        if (percentage == 1f && isHideToolbarView) {
             date_behavior.setVisibility(View.GONE);
             titleAppbar.setVisibility(View.VISIBLE);
-            isHideTolbarView = !isHideTolbarView;
+            isHideToolbarView = !isHideToolbarView;
         }
 
-        else if (percentage < 1f && isHideTolbarView) {
+        else if (percentage < 1f && isHideToolbarView) {
             date_behavior.setVisibility(View.VISIBLE);
             titleAppbar.setVisibility(View.GONE);
-            isHideTolbarView = !isHideTolbarView;
+            isHideToolbarView = !isHideToolbarView;
         }
     }
 }
