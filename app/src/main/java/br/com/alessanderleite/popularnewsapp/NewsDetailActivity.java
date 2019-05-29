50400689c50ca6cafdef30c6e9dbf89b391277ed
alessanderleite <alessander.logistica@gmail.com>
2019-05-29 16:10:19 -0300

    code the DetailActivity class

--- app/src/main/java/br/com/alessanderleite/popularnewsapp/NewsDetailActivity.java
+++ app/src/main/java/br/com/alessanderleite/popularnewsapp/NewsDetailActivity.java
@@ -1,13 +1,94 @@
 package br.com.alessanderleite.popularnewsapp;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class NewsDetailActivity extends AppCompatActivity {
+public class NewsDetailActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener{
+
+    private ImageView imageView;
+    private TextView appbar_title, appbar_subtitle, date, time, title;
+    private boolean isHideTolbarView = false;
+    private FrameLayout date_behavior;
+    private LinearLayout titleAppbar;
+    private AppBarLayout appBarLayout;
+    private Toolbar toolbar;
+    private String mUrl, mImg, mTitle, mDate, mSource, mAuthor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+
+        appBarLayout = findViewById(R.id.appbar);
+        appBarLayout.addOnOffsetChangedListener(this);
+        date_behavior = findViewById(R.id.date_behavior);
+        titleAppbar = findViewById(R.id.title_appbar);
+        imageView = findViewById(R.id.backdrop);
+        appbar_title = findViewById(R.id.title_on_appbar);
+        appbar_subtitle = findViewById(R.id.subtitle_on_appbar);
+        date = findViewById(R.id.date);
+        time = findViewById(R.id.time);
+        title = findViewById(R.id.title);
+    }
+
+    private void initWebView(String url) {
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        supportFinishAfterTransition();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(verticalOffset) / (float) maxScroll;
+
+        if (percentage == 1f && isHideTolbarView) {
+            date_behavior.setVisibility(View.GONE);
+            titleAppbar.setVisibility(View.VISIBLE);
+            isHideTolbarView = !isHideTolbarView;
+        }
+
+        else if (percentage < 1f && isHideTolbarView) {
+            date_behavior.setVisibility(View.VISIBLE);
+            titleAppbar.setVisibility(View.GONE);
+            isHideTolbarView = !isHideTolbarView;
+        }
     }
 }
